5cca8d1b79bc54c8d556a7c9176791d43015add7
YoussefElsaygh <youssefelsaygh@gmail.com>
2020-07-06 12:40:30 +0200

    Update simple_rsa.dart
    dart add abstract class for avaiable sha algorithim and modify verify and sign functions

--- lib/simple_rsa.dart
+++ lib/simple_rsa.dart
@@ -1,10 +1,15 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-
+abstract class RSAHash{
+  static const String sha512='SHA512withRSA';
+  static const String sha224='SHA224withRSA';
+  static const String sha256='SHA256withRSA';
+  static const String sha384='SHA384withRSA';
+  static const String sha1='SHA1withRSA';
+}
 const MethodChannel _channel =
     const MethodChannel('juanito21.com/simple_rsa');
-
 Future<String> encryptString(String txt, String publicKey) async {
   try {
     publicKey = publicKey.replaceAll("-----BEGIN PUBLIC KEY-----", "").replaceAll("-----END PUBLIC KEY-----", "");
@@ -28,23 +33,23 @@ Future<String> decryptString(String txt, String privateKey) async {
   }
 }
 
-Future<String> signString(String plainText, String privateKey) async {
+Future<String> signString(String plainText, String privateKey,[String sha=RSAHash.sha1]) async {
   try {
     privateKey = privateKey.replaceAll("-----BEGIN PRIVATE KEY-----", "").replaceAll("-----END PRIVATE KEY-----", "");
     privateKey = privateKey.replaceAll("-----BEGIN RSA PRIVATE KEY-----", "").replaceAll("-----END RSA PRIVATE KEY-----", "");
     final String result = await _channel
-        .invokeMethod('sign', {"plainText": plainText, "privateKey": privateKey});
+        .invokeMethod('sign', {"plainText": plainText, "privateKey": privateKey,"sha":sha});
     return result;
   } on PlatformException catch (e) {
     throw "Failed decoded string: '${e.message}'.";
   }
 }
 
-Future<bool> verifyString(String plainText, String signature, String publicKey) async {
+Future<bool> verifyString(String plainText, String signature, String publicKey,[String sha=RSAHash.sha1]) async {
   try {
     publicKey = publicKey.replaceAll("-----BEGIN PUBLIC KEY-----", "").replaceAll("-----END PUBLIC KEY-----", "");
     final bool result = await _channel
-        .invokeMethod('verify', {"plainText": plainText, "signature": signature, "publicKey": publicKey});
+        .invokeMethod('verify', {"plainText": plainText, "signature": signature, "publicKey": publicKey,"sha":sha});
     return result;
   } on PlatformException catch (e) {
     throw "Failed decoded string: '${e.message}'.";
